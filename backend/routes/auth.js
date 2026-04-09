d8ce7cab3bad7ac97aed9943e27dcd41e13bbfa9
sanjeevkoshti <koshtisanjeev6@gmail.com>
2026-04-09 10:35:40 +0530

    correction the otp with gmail smtp

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,7 +11,9 @@ const SMTP_PASSWORD = process.env.SMTP_PASSWORD || '';
 // Create transporter
 function createTransporter() {
   return nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: SMTP_EMAIL,
       pass: SMTP_PASSWORD
